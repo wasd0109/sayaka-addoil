7428716ec0f7076eba437d104bc3eb30d7b50f05
KinChing CHEUNG <kinching.a.cheung@rakuten.com>
2023-08-22 00:36:57 +0900

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '掛橋沙耶香集氣區',
   description: 'Created by Ken Cheung @monakaken',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-nogizaka`}>{children}</body>
+      <Analytics />
     </html>
-  )
+
+  );
 }
