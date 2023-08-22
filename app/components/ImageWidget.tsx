0fe56699117781780ac3c34d6cadd8aa2b4f9863
KinChing CHEUNG <kinching.a.cheung@rakuten.com>
2023-08-22 12:00:15 +0900

    Revert "check"
    This reverts commit 4fe495c17ccadea9634dc51e43a81f824c70168b.

--- app/components/ImageWidget.tsx
+++ app/components/ImageWidget.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 
 
 export default function ImageWidget() {
     return (
-        <div className='card'>
-            <Image className='card-body' src="/sayaka_1.jpg" height={1000} width={1000} alt={'Sayaka'} priority={true}></Image>
-        </div>);
+        <Image src="/sayaka_1.jpg" height={1000} width={1000} alt={'Sayaka'} priority={true}></Image>
+    )
 }
