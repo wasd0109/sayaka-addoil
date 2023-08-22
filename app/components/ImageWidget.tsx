efea7852e83431de5e37b6ba2114a78246db528a
KinChing CHEUNG <kinching.a.cheung@rakuten.com>
2023-08-22 12:05:19 +0900

    Minor style change

--- app/components/ImageWidget.tsx
+++ app/components/ImageWidget.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 
 export default function ImageWidget() {
     return (
-        <Image src="/sayaka_1.jpg" height={1000} width={1000} alt={'Sayaka'} priority={true}></Image>
-    )
+        <Image className='rounded-md' src="/sayaka_1.jpg" height={300} width={300} alt={'Sayaka'} priority={true}></Image>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-"use client"
-import { useCookies } from 'react-cookie'
-import AddOilWidget from './components/AddOilWidget'
-import DateWidget from './components/DateWidget'
-import ImageWidget from './components/ImageWidget'
-import { use, useEffect } from 'react'
+"use client";
+import { useCookies } from 'react-cookie';
+import AddOilWidget from './components/AddOilWidget';
+import DateWidget from './components/DateWidget';
+import ImageWidget from './components/ImageWidget';
+import { use, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { useAddOil, useGetAddOilNumber } from './api/useApi'
-import { Toaster, toast } from 'react-hot-toast'
+import { useAddOil, useGetAddOilNumber } from './api/useApi';
+import { Toaster, toast } from 'react-hot-toast';
 
 export default function Home() {
   const [cookies, setCookie] = useCookies(["uuid"]);
@@ -18,21 +18,21 @@ export default function Home() {
       const uuid = uuidv4();
       setCookie("uuid", uuid);
     }
-  }, [cookies, setCookie])
+  }, [cookies, setCookie]);
 
 
-  console.log(cookies)
+  console.log(cookies);
 
   return (
-    <main className="flex min-h-dscreen max-h-dscreen flex-col items-center justify-around p-4 m-4 bg-slate-100 font-roboto">
+    <main className="flex flex-col items-center justify-around p-4 m-4 bg-slate-100 font-roboto rounded-md shadow-md" >
       <div><Toaster /></div>
       <DateWidget />
       <ImageWidget />
       <button className='bg-nogizaka text-white p-4  mt-4 mb-2 rounded-xl' onClick={() => {
-        const addOilPromise = addOil()
+        const addOilPromise = addOil();
         toast.promise(addOilPromise, { loading: "集氣中", success: "成功集氣", error: (error) => error.toString() });
       }}>為掛橋集氣</button>
       <AddOilWidget total={total} todayTotal={todayTotal} loading={loading} />
     </main>
-  )
+  );
 }
